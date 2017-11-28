8e2690fc25bc8b73aa53dfdae2fea90d3b9b5afb
Pierre <pierre.fervault@laposte.net>
2017-11-28 17:06:13 +0100

    Ajout Conseiller

---
+++ src/Conseiller.java
@@ -0,0 +1,12 @@
+public class Conseiller {
+
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
