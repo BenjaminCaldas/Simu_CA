4181bf713461eb65a1437721b9c3d036822e63ca
sciencus <logan.verecque1@laposte.net>
2017-11-28 17:58:45 +0100

    Create Aire class, Beginning of class Simulation

---
+++ src/Simulation.java
@@ -0,0 +1,21 @@
+public class Simulation
+{
+    public Simulation()
+    {
+
+    }
+
+    public void Debut()
+    {
+        Aire aires = new Aire();
+        CentreAppel centreAppel = new CentreAppel(0, 0, 0);
+
+
+    }
+
+    public void Fin()
+    {
+
+    }
+
+}
