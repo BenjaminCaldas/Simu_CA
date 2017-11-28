461414f8c068db5109a13a123e9942d800a88545
Pierre <pierre.fervault@laposte.net>
2017-11-28 18:01:01 +0100

    Ajout methode telephone, accès et arrivée

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+
+
 public class Main {
 
+    public void arriveeTelephone(CentreAppel centreAppel){
+        ajouterEvenement(centreAppel.getTempsActuel());
+
+        if((centreAppel.getN() - centreAppel.getBt() - centreAppel.getBc() == 0) ||
+                (centreAppel.getQt() > centreAppel.getBt() && centreAppel.getBt() < centreAppel.getT())){
+            centreAppel.incrementerFileTelephone();
+        }
+        else{
+            ajouterEvenement(centreAppel.getTempsActuel());
+        }
+        derniereDateSimu = dateSimu;
+    }
+
+    public void accesTelephone(CentreAppel centreAppel){
+        ajouterEvenement(centreAppel.getTempsActuel());
+        centreAppel.enleverConseillerCourriels();
+        mettreAireAjour();
+        derniereDateSimu = dateSimu;
+    }
+
+    
+
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
